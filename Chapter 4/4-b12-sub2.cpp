95729d0b440131e0d5d3e36472b26efdd081d4d5
harrypotterrrr <jiahaolin19971119@126.com>
2018-07-17 23:53:15 +0800

    remove

--- Chapter 4/4-b12-sub2.cpp
+++
@@ -1,9 +0,0 @@
-/* 1班 贾昊霖 1651574 */
-#include <iostream>
-using namespace std;
-void DeltaGreaterZero(double pre, double rear)
-{
-	cout << "有两个不等实根：\n"
-		<< "x1=" << pre + rear << "\n"
-		<< "x2=" << pre - rear << "\n";
-}
